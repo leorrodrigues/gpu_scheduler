d7bfe5425bff5fc9ba78a9eeb15ce46f1d380e4c
leonardo <leu-rr1@hotmail.com>
2018-12-13 17:18:57 -0200

    cleaning

--- gpuScheduler/multicriteria/ahp.cpp
+++ gpuScheduler/multicriteria/ahp.cpp
@@ -475,7 +475,7 @@ void AHP::conception(bool alternativeParser) {
 	strcpy(hierarchy_data, path);
 
 	strcat(hierarchy_schema, "/multicriteria/json/hierarchySchema.json");
-	strcat(hierarchy_data, "/multicriteria/json/hierarchyDataFrag.json");
+	strcat(hierarchy_data, "/multicriteria/json/hierarchyData.json");
 
 	rapidjson::SchemaDocument hierarchySchema =
 		JSON::generateSchema(hierarchy_schema);
