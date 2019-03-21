d0f20b4d2e78317fc967a7cf330a631e4c0acc7a
Leonardo Rosa Rodrigues <leu_rr1@hotmail.com>
2019-03-21 13:14:39 +0100

    update

--- tests/container_links/run_test.sh
+++ tests/container_links/run_test.sh
@@ -1,17 +1,18 @@
 #echo "Multicriteria method;Fat Tree Size;Number of containers;Time" >> times.txt
 #for m in ahp_clusterized; do
 cd ./../../scheduler/;
-for dt in flat bw; do
+ft=4
+for dt in flat frag bw; do
 	for g in none mcl; do
 		for p in 0 50 100; do
 	        	for bw in 0 25 50; do
 				for m in ahpg topsis; do
-					echo "Fat Tree k= 44 Algoritmo $m Cluster $g Request-size $rs"
-		        	       ./gpuscheduler.out --test 4 -s 44 -m $m -c $g --request-size $p --bw $bw --data-type $dt
+					echo "Fat Tree k=$ft Algoritmo $m Cluster $g Request-size $rs"
+		        	       ./gpuscheduler.out --test 4 -s $ft -m $m -c $g --request-size $p --bw $bw --data-type $dt --debug error
 		      		done;
-	        	   	for sm in best_fit worst_fit first_fit; do
-                			echo "Fat Tree k= 44 Algoritmo $sm Cluster $g Request-size $rs"
-		               		./gpuscheduler.out --test 4 -s 44 --standard-allocation $sm -c $g --request-size $p --bw $bw --data-type $dt
+	        	   	for sm in best_fit worst_fit; do # first_fit; do
+                			echo "Fat Tree k=$ft Algoritmo $sm Cluster $g Request-size $rs"
+		               		./gpuscheduler.out --test 4 -s $ft --standard-allocation $sm -c $g --request-size $p --bw $bw --data-type $dt --debug error
 		       	 	done;
 		       done;
 		done;
