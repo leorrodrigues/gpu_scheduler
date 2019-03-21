3b19ac6ae0950dc80f8c7922a03bb619546c47ac
Leonardo Rosa Rodrigues <leu_rr1@hotmail.com>
2019-03-21 15:09:40 +0100

    update

--- tests/container_links/run_4_test.sh
+++ tests/container_links/run_4_test.sh
@@ -1,16 +1,12 @@
 #echo "Multicriteria method;Fat Tree Size;Number of containers;Time" >> times.txt
 #for m in ahp_clusterized; do
 cd ./../../scheduler/;
-ft=44
-for dt in flat frag bw; do
-	for g in none mcl; do
+ft=4
+for dt in flat; do
+	for g in none; do
 		for p in 0 50 100; do
 	        	for bw in 0 25 50; do
-				for m in ahpg topsis; do
-					echo "Fat Tree k=$ft Algoritmo $m Cluster $g Request-size $rs"
-		        	       ./gpuscheduler.out --test 4 -s $ft -m $m -c $g --request-size $p --bw $bw --data-type $dt
-		      		done;
-	        	   	for sm in best_fit worst_fit; do # first_fit; do
+				for sm in best_fit worst_fit; do # first_fit; do
                 			echo "Fat Tree k=$ft Algoritmo $sm Cluster $g Request-size $rs"
 		               		./gpuscheduler.out --test 4 -s $ft --standard-allocation $sm -c $g --request-size $p --bw $bw --data-type $dt
 		       	 	done;
