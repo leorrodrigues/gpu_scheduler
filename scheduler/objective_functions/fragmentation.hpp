9b1e5dc3c89461baeb30f0a9a06606e1974a806f
leonardo <leu_rr1@hotmail.com>
2019-03-30 18:07:43 +0100

    update the min value to 1\^(-10)

--- scheduler/objective_functions/fragmentation.hpp
+++ scheduler/objective_functions/fragmentation.hpp
@@ -10,23 +10,23 @@ namespace Fragmentation {
 
 inline float datacenter(consumed_resource_t consumed,total_resources_t total){
 	float value = (consumed.active_servers/ (float)total.servers);
-	return (value >= 0.00000000001) ? value : 0;
+	return (value >= 0.0000000001) ? value : 0;
 }
 
 inline float datacenter(unsigned int active, unsigned int total){
 	float value =  active/(float)total;
-	return (value >= 0.00000000001) ? value : 0;
+	return (value >= 0.0000000001) ? value : 0;
 }
 
 inline float link(consumed_resource_t consumed, total_resources_t total){
 	float value =  consumed.active_links/ (float) total.links;
-	return (value >= 0.00000000001) ? value : 0;
+	return (value >= 0.0000000001) ? value : 0;
 
 }
 
 inline float link(unsigned int active, unsigned int total){
 	float value =  active/(float)total;
-	return (value >= 0.00000000001) ? value : 0;
+	return (value >= 0.0000000001) ? value : 0;
 }
 
 }
