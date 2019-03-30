2e728393e635a4b7ddc601c39e8bf7f4cbd48e85
leonardo <leu_rr1@hotmail.com>
2019-03-30 18:19:42 +0100

    remove bw 25

--- tests/container_links/run_test.sh
+++ tests/container_links/run_test.sh
@@ -5,7 +5,7 @@ ft=44
 for dt in flat frag bw; do
 	for g in none mcl; do
 		for p in 0 50 100; do
-	        	for bw in 0 25 50; do
+	        	for bw in 0 50; do
 				for m in ahpg topsis; do
 					echo "Fat Tree k=$ft Algoritmo $m Cluster $g Request-size $rs"
 		        	       ./gpuscheduler.out --test 4 -s $ft -m $m -c $g --request-size $p --bw $bw --data-type $dt
