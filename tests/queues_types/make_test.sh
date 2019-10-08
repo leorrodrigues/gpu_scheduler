9ec326b821d7ce80fd7f42dc557a784879e4cc81
leonardo <leu_rr1@hotmail.com>
2019-10-08 08:32:56 -0300

    gtx1070 online and offline tests

---
+++ tests/queues_types/make_test.sh
@@ -0,0 +1,9 @@
+cd ./../../bin/;
+onoff="online"
+size="50k"
+for queue in {"fcfs","spf","sqfmin","safmin","sdafmin"}; do
+    for site in "lyon"; do
+        echo "Executing scheduler with file $size, type $onoff and queue $queue"
+        ./gpu_scheduler.out --test 5 --data-type flat -c none --file_name $site-$size -r ahpg --scheduling_type $onoff --debug critical;
+    done;
+done;
