78f8d8af5f0c1027acc1a0152556b205ee3fad69
leonardo <leu_rr1@hotmail.com>
2019-10-09 14:54:23 -0300

    update

--- tests/queues_types/make_test.sh
+++ tests/queues_types/make_test.sh
@@ -1,9 +1,12 @@
 cd ./../../bin/;
-onoff="online"
-size="50k"
-for queue in {"fcfs","spf","sqfmin","safmin"}; do
-    for site in "lyon"; do
-        echo "Executing scheduler with file $size, type $onoff and queue $queue"
-        ./gpu_scheduler.out --test 5 --data-type flat -c none --file_name $site-$size -r ahpg --scheduling_type $onoff --debug critical --cmp $queue;
+
+for site in "lyon"; do
+    for onoff in {"online","offline"}; do
+        for queue in {"fcfs","spf","sqfmin","safmin"}; do
+            for size in {"150k","100k","50k"}; do
+                echo "Executing scheduler with file $site-$size, type $onoff and queue $queue"
+                ./gpu_scheduler.out --test 5 --data-type flat -c none --file_name $site-$size -r ahpg --scheduling_type $onoff --debug critical --cmp $queue;
+            done;
+        done;
     done;
 done;
