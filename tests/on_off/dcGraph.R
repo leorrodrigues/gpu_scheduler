280b707aa02ff704fbb09d92044ad94588e6631b
leonardo <leu_rr1@hotmail.com>
2019-10-09 19:34:26 -0300

    update the graphs functions

--- tests/on_off/dcGraph.R
+++ tests/on_off/dcGraph.R
@@ -16,143 +16,146 @@ pallet_colors <- c("#00AFBB", "#E7B800", "#FC4E07","#52854C","#FFDB6D","#4E84C4"
 is.nan.data.frame <- function(x)
     do.call(cbind, lapply(x, is.nan))
 
-#######################################
-## 1070 TI ##
-#######################################
-gtxDataOnline <- read.table("data/gtx1070ti/dc-none-lyon-50k-fcfs-online-lyon-50k.log",header=FALSE,sep=" ")
-gtxDataOffline <- read.table("data/gtx1070ti/dc-none-lyon-50k-fcfs-offline-lyon-50k.log",header=FALSE,sep=" ")
-
-gtxDataOnline$type = "online"
-gtxDataOffline$type = "offline"
-
-gtxData <- rbind(gtxDataOnline, gtxDataOffline)
-
-rm(gtxDataOnline)
-rm(gtxDataOffline)
-
-names(gtxData) <- c("method","time","dc_fragmentation","footprint_vcpu","footprint_ram","fragmentation_link","footprint_link","footprint_bandwidth","rejected_tasks","accepted_tasks","accepted_performance", "type")
-gtxData$gpu <- "GTX 1070 Ti"
-
-#######################################
-## Titan XP ##
-#######################################
-titanXPDataOnline <- read.table("data/titanXP/dc-none-lyon-50k-fcfs-online-lyon-50k.log",header=FALSE,sep=" ")
-titanXPDataOffline <- read.table("data/titanXP/dc-none-lyon-50k-fcfs-offline-lyon-50k.log",header=FALSE,sep=" ")
-
-titanXPDataOnline$type = "online"
-titanXPDataOffline$type = "offline"
-
-titanXPData <- rbind(titanXPDataOnline, titanXPDataOffline)
-
-rm(titanXPDataOnline)
-rm(titanXPDataOffline)
-
-names(titanXPData) <- c("method","time","dc_fragmentation","footprint_vcpu","footprint_ram","fragmentation_link","footprint_link","footprint_bandwidth","rejected_tasks","accepted_tasks","accepted_performance", "type")
-titanXPData$gpu <- "Titan XP"
-
-#######################################
-## 2080 TI ##
-#######################################
-rtxDataOnline <- read.table("data/rtx2080ti/dc-none-lyon-50k-fcfs-online-lyon-50k.log",header=FALSE,sep=" ")
-rtxDataOffline <- read.table("data/rtx2080ti/dc-none-lyon-50k-fcfs-offline-lyon-50k.log",header=FALSE,sep=" ")
-
-rtxDataOnline$type = "online"
-rtxDataOffline$type = "offline"
-
-rtxData <- rbind(rtxDataOnline, rtxDataOffline)
-
-rm(rtxDataOnline)
-rm(rtxDataOffline)
-
-names(rtxData) <- c("method","time","dc_fragmentation","footprint_vcpu","footprint_ram","fragmentation_link","footprint_link","footprint_bandwidth","rejected_tasks","accepted_tasks","accepted_performance", "type")
-rtxData$gpu <- "RTX 2080 Ti"
-
-#######################################
-## Merge all the data ##
-#######################################
-
-dt <- rbind(gtxData, titanXPData)
-dt <- rbind(dt, rtxData)
-
-rm(gtxData)
-rm(titanXPData)
-rm(rtxData)
-
-dt$accepted_performance <- NULL
-dt$accepted_performance <- with(dt, accepted_tasks / (accepted_tasks+rejected_tasks))
-dt[is.nan(dt)] <- 0
-
-#######################################
-## Plot the accepted graph ##
-#######################################
-acceptedPlot <- ggplot()+
-    geom_line(data=dt, aes(x=time, y=accepted_performance, color=type)) +
-    theme_classic()+
-    theme(
-        legend.position="top",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12,
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x=("Time(s)"),
-        y="Accepted Ratio (%)",
-        colour="Scheduling Type"
-    )+
-    scale_color_d3()
-
-#######################################
-## Plot the boxplot graph ##
-#######################################
-footVcpuPlot <- ggplot()+
-    geom_line(data=dt, aes(x=time, y=footprint_vcpu, color=type)) +
-    theme_classic()+
-    theme(
-        legend.position="top",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12,
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x=("Time(s)"),
-        y="Footprint vCPU (%)",
-        colour="Scheduling Type"
-    )+
-    scale_color_d3()
-
-###################################
-## Plot all the graphs
-###################################
-
-tiff("accepted_performance_50k.tiff",width= 3000, height= 1200, units="px", res=400,compression = 'lzw')
-plot(acceptedPlot)
-dev.off()
-
-tiff("footprint_vcpu_50k.tiff",width= 3000, height= 1200, units="px", res=400,compression = 'lzw')
-plot(footVcpuPlot)
-dev.off()
+sizes = c("50k","100k","150k")
+for(size in sizes){
+    #######################################
+    ## 1070 TI ##
+    #######################################
+    gtxDataOnline <- read.table(sprintf("data/gtx1070ti/dc-none-lyon-%s-fcfs-online-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    gtxDataOffline <- read.table(sprintf("data/gtx1070ti/dc-none-lyon-%s-fcfs-offline-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    
+    gtxDataOnline$type = "online"
+    gtxDataOffline$type = "offline"
+    
+    gtxData <- rbind(gtxDataOnline, gtxDataOffline)
+    
+    rm(gtxDataOnline)
+    rm(gtxDataOffline)
+    
+    names(gtxData) <- c("method","time","dc_fragmentation","footprint_vcpu","footprint_ram","fragmentation_link","footprint_link","footprint_bandwidth","rejected_tasks","accepted_tasks","accepted_performance", "type")
+    gtxData$gpu <- "GTX 1070 Ti"
+    
+    #######################################
+    ## Titan XP ##
+    #######################################
+    titanXPDataOnline <- read.table(sprintf("data/titanXP/dc-none-lyon-%s-fcfs-online-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    titanXPDataOffline <- read.table(sprintf("data/titanXP/dc-none-lyon-%s-fcfs-offline-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    
+    titanXPDataOnline$type = "online"
+    titanXPDataOffline$type = "offline"
+    
+    titanXPData <- rbind(titanXPDataOnline, titanXPDataOffline)
+    
+    rm(titanXPDataOnline)
+    rm(titanXPDataOffline)
+    
+    names(titanXPData) <- c("method","time","dc_fragmentation","footprint_vcpu","footprint_ram","fragmentation_link","footprint_link","footprint_bandwidth","rejected_tasks","accepted_tasks","accepted_performance", "type")
+    titanXPData$gpu <- "Titan XP"
+    
+    #######################################
+    ## 2080 TI ##
+    #######################################
+    rtxDataOnline <- read.table(sprintf("data/rtx2080ti/dc-none-lyon-%s-fcfs-online-lyon-%s.log",size, size),header=FALSE,sep=" ")
+    rtxDataOffline <- read.table(sprintf("data/rtx2080ti/dc-none-lyon-%s-fcfs-offline-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    
+    rtxDataOnline$type = "online"
+    rtxDataOffline$type = "offline"
+    
+    rtxData <- rbind(rtxDataOnline, rtxDataOffline)
+    
+    rm(rtxDataOnline)
+    rm(rtxDataOffline)
+    
+    names(rtxData) <- c("method","time","dc_fragmentation","footprint_vcpu","footprint_ram","fragmentation_link","footprint_link","footprint_bandwidth","rejected_tasks","accepted_tasks","accepted_performance", "type")
+    rtxData$gpu <- "RTX 2080 Ti"
+    
+    #######################################
+    ## Merge all the data ##
+    #######################################
+    
+    dt <- rbind(gtxData, titanXPData)
+    dt <- rbind(dt, rtxData)
+    
+    rm(gtxData)
+    rm(titanXPData)
+    rm(rtxData)
+    
+    dt$accepted_performance <- NULL
+    dt$accepted_performance <- with(dt, accepted_tasks / (accepted_tasks+rejected_tasks))
+    dt[is.nan(dt)] <- 0
+    
+    #######################################
+    ## Plot the accepted graph ##
+    #######################################
+    acceptedPlot <- ggplot()+
+        geom_line(data=dt, aes(x=time, y=accepted_performance, color=type)) +
+        theme_classic()+
+        theme(
+            legend.position="top",
+            axis.text.x = element_text(
+                angle = 0,
+                hjust = 0.7,
+                size=12,
+            ),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"),
+            axis.text.y = element_text(size=12), 
+            axis.title.x = element_text(size=12),
+            axis.title.y = element_text(size=12),
+            legend.text = element_text(size=12),
+            legend.title = element_text(size=12)
+        )+
+        labs(
+            x=("Time(s)"),
+            y="Acceptance Ratio (%)",
+            colour="Scheduling Type"
+        )+
+        scale_color_d3()
+    
+    #######################################
+    ## Plot the boxplot graph ##
+    #######################################
+    footVcpuPlot <- ggplot(data=dt, aes(x=time, y=footprint_vcpu, color=type))+
+        geom_line() +
+        theme_classic()+
+        theme(
+            legend.position="top",
+            axis.text.x = element_text(
+                angle = 0,
+                hjust = 0.7,
+                size=12,
+            ),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"),
+            axis.text.y = element_text(size=12), 
+            axis.title.x = element_text(size=12),
+            axis.title.y = element_text(size=12),
+            legend.text = element_text(size=12),
+            legend.title = element_text(size=12)
+        )+
+        labs(
+            x=("Time(s)"),
+            y="Footprint vCPU (%)",
+            colour="Scheduling Type"
+        )+
+        scale_color_d3()
+    
+    ###################################
+    ## Plot all the graphs
+    ###################################
+    
+    tiff(sprintf("accepted_performance_%s.tiff",size),width= 3000, height= 1200, units="px", res=400,compression = 'lzw')
+    plot(acceptedPlot)
+    dev.off()
+    
+    tiff(sprintf("footprint_vcpu_%s.tiff",size),width= 3000, height= 1200, units="px", res=400,compression = 'lzw')
+    plot(footVcpuPlot)
+    dev.off()
+}
 #################################################
 ## CONVERTING THE FINAL GRAPHS INTO PNG AND EPS##
 #################################################
--- tests/on_off/requestGraph.R
+++ tests/on_off/requestGraph.R
@@ -16,246 +16,178 @@ pallet_colors <- c("#00AFBB", "#E7B800", "#FC4E07","#52854C","#FFDB6D","#4E84C4"
 is.nan.data.frame <- function(x)
     do.call(cbind, lapply(x, is.nan))
 
-#######################################
-## 1070 TI ##
-#######################################
-gtxDataOnline <- read.table("data/gtx1070ti/request-none-lyon-50k-fcfs-online-lyon-50k.log",header=FALSE,sep=" ")
-gtxDataOffline <- read.table("data/gtx1070ti/request-none-lyon-50k-fcfs-offline-lyon-50k.log",header=FALSE,sep=" ")
-
-gtxDataOnline$type = "online"
-gtxDataOffline$type = "offline"
-
-gtxData <- rbind(gtxDataOnline, gtxDataOffline)
-
-rm(gtxDataOnline)
-rm(gtxDataOffline)
-
-names(gtxData) <- c("method","submission","id","delay","task_utility","link_utility","walltime","delay_link","footprint_bandwidth","type")
-gtxData$gpu <- "GTX 1070 Ti"
-
-#######################################
-## Titan XP ##
-#######################################
-titanXPDataOnline <- read.table("data/titanXP/request-none-lyon-50k-fcfs-online-lyon-50k.log",header=FALSE,sep=" ")
-titanXPDataOffline <- read.table("data/titanXP/request-none-lyon-50k-fcfs-offline-lyon-50k.log",header=FALSE,sep=" ")
-
-titanXPDataOnline$type = "online"
-titanXPDataOffline$type = "offline"
-
-titanXPData <- rbind(titanXPDataOnline, titanXPDataOffline)
-
-rm(titanXPDataOnline)
-rm(titanXPDataOffline)
-
-names(titanXPData) <- c("method","submission","id","delay","task_utility","link_utility","walltime","delay_link","footprint_bandwidth","type")
-titanXPData$gpu <- "Titan XP"
-
-#######################################
-## 2080 TI ##
-#######################################
-rtxDataOnline <- read.table("data/rtx2080ti/request-none-lyon-50k-fcfs-online-lyon-50k.log",header=FALSE,sep=" ")
-rtxDataOffline <- read.table("data/rtx2080ti/request-none-lyon-50k-fcfs-offline-lyon-50k.log",header=FALSE,sep=" ")
-
-rtxDataOnline$type = "online"
-rtxDataOffline$type = "offline"
-
-rtxData <- rbind(rtxDataOnline, rtxDataOffline)
-
-rm(rtxDataOnline)
-rm(rtxDataOffline)
-
-names(rtxData) <- c("method","submission","id","delay","task_utility","link_utility","walltime","delay_link","footprint_bandwidth","type")
-rtxData$gpu <- "RTX 2080 Ti"
-
-#######################################
-## Merge all the data ##
-#######################################
-
-dt <- rbind(gtxData, titanXPData)
-dt <- rbind(dt, rtxData)
-
-rm(gtxData)
-rm(titanXPData)
-rm(rtxData)
-
-#######################################
-## Plot the walltime line graph ##
-#######################################
-walltimeLinePlot <- ggplot(data=dt, aes( x=submission, y=walltime, color= type, linetype= gpu))+
-    # data=dt, aes( x=submission, y=walltime, color= interaction(type, gpu)))+
-    geom_line() +
-    theme_classic()+
-    theme(
-        legend.position="right",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x="Submission (Events)",
-        y="Walltime (s)",
-        color="Scheduling Type",
-        linetype="GPU"
-    )+
-    scale_color_d3()
-
-#######################################
-## Plot the walltime cdf graph ##
-#######################################
-walltimeCDFPlot <- ggplot(data=dt, aes( walltime, color= type, linetype= gpu))+
-    stat_ecdf(geom="step") +
-    theme_classic()+
-    theme(
-        legend.position="right",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x="Walltime",
-        y="CDF (%)",
-        color="Scheduling Type",
-        linetype="GPU"
-    )+
-    scale_color_d3()
-
-#######################################
-## Plot the delay line graph ##
-#######################################
-delayLinePlot <- ggplot(data=dt, aes( x=submission, y=delay, color= type))+
-    # data=dt, aes( x=submission, y=walltime, color= interaction(type, gpu)))+
-    geom_line() +
-    theme_classic()+
-    theme(
-        legend.position="right",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x="Submission (Events)",
-        y="Delay (Events)",
-        color="Scheduling Type"
-    )+
-    scale_color_d3()
-
-#######################################
-## Plot the delay cdf graph ##
-#######################################
-delayCDFPlot <- ggplot(data=dt, aes( delay, color= type))+
-    stat_ecdf(geom="step") +
-    theme_classic()+
-    theme(
-        legend.position="right",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x="Delay (Events)",
-        y="CDF (%)",
-        color="Scheduling Type"
-    )+
-    scale_color_d3()
-
-#######################################
-## Plot the walltime by delay graph ##
-#######################################
-walltimeByDelayPlot <- ggplot(data=dt, aes( y= delay, x= walltime, color= type, linetype = gpu))+
-    geom_point() +
-    theme_classic()+
-    theme(
-        legend.position="right",
-        axis.text.x = element_text(
-            angle = 0,
-            hjust = 0.7,
-            size=12
-        ),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text.y = element_text(size=12), 
-        axis.title.x = element_text(size=12),
-        axis.title.y = element_text(size=12),
-        legend.text = element_text(size=12),
-        legend.title = element_text(size=12)
-    )+
-    labs(
-        x="Delay (Events)",
-        y="Walltime (s)",
-        color="Scheduling Type",
-        linetype = "GPU"
-    )+
-    scale_color_d3()
-
-###################################
-## Plot all the graphs
-###################################
-
-tiff("walltime_line_50k.tiff",width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
-plot(walltimeLinePlot)
-dev.off()
-
-tiff("walltime_cdf_50k.tiff",width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
-plot(walltimeCDFPlot)
-dev.off()
-
-tiff("delay_line_50k.tiff",width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
-plot(delayLinePlot)
-dev.off()
-
-tiff("delay_cdf_50k.tiff",width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
-plot(delayCDFPlot)
-dev.off()
-
-tiff("walltime_delay_50k.tiff",width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
-plot(walltimeByDelayPlot)
-dev.off()
+sizes = c("50k","100k","150k")
+for(size in sizes){
+    #######################################
+    ## 1070 TI ##
+    #######################################
+    gtxDataOnline <- read.table(sprintf("data/gtx1070ti/request-none-lyon-%s-fcfs-online-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    gtxDataOffline <- read.table(sprintf("data/gtx1070ti/request-none-lyon-%s-fcfs-offline-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    
+    gtxDataOnline$type = "online"
+    gtxDataOffline$type = "offline"
+    
+    gtxData <- rbind(gtxDataOnline, gtxDataOffline)
+    
+    rm(gtxDataOnline)
+    rm(gtxDataOffline)
+    
+    names(gtxData) <- c("method","submission","id","delay","task_utility","link_utility","walltime","delay_link","footprint_bandwidth","type")
+    gtxData$gpu <- "GTX 1070 Ti"
+    
+    #######################################
+    ## Titan XP ##
+    #######################################
+    titanXPDataOnline <- read.table(sprintf("data/titanXP/request-none-lyon-%s-fcfs-online-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    titanXPDataOffline <- read.table(sprintf("data/titanXP/request-none-lyon-%s-fcfs-offline-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    
+    titanXPDataOnline$type = "online"
+    titanXPDataOffline$type = "offline"
+    
+    titanXPData <- rbind(titanXPDataOnline, titanXPDataOffline)
+    
+    rm(titanXPDataOnline)
+    rm(titanXPDataOffline)
+    
+    names(titanXPData) <- c("method","submission","id","delay","task_utility","link_utility","walltime","delay_link","footprint_bandwidth","type")
+    titanXPData$gpu <- "Titan XP"
+    
+    #######################################
+    ## 2080 TI ##
+    #######################################
+    rtxDataOnline <- read.table(sprintf("data/rtx2080ti/request-none-lyon-%s-fcfs-online-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    rtxDataOffline <- read.table(sprintf("data/rtx2080ti/request-none-lyon-%s-fcfs-offline-lyon-%s.log",size,size),header=FALSE,sep=" ")
+    
+    rtxDataOnline$type = "online"
+    rtxDataOffline$type = "offline"
+    
+    rtxData <- rbind(rtxDataOnline, rtxDataOffline)
+    
+    rm(rtxDataOnline)
+    rm(rtxDataOffline)
+    
+    names(rtxData) <- c("method","submission","id","delay","task_utility","link_utility","walltime","delay_link","footprint_bandwidth","type")
+    rtxData$gpu <- "RTX 2080 Ti"
+    
+    #######################################
+    ## Merge all the data ##
+    #######################################
+    
+    dt <- rbind(gtxData, titanXPData)
+    dt <- rbind(dt, rtxData)
+    
+    rm(gtxData)
+    rm(titanXPData)
+    rm(rtxData)
+    
+    #######################################
+    ## Plot the walltime cdf graph ##
+    #######################################
+    walltimeCDFPlot <- ggplot(data=dt, aes( walltime, color= type, linetype= gpu))+
+        stat_ecdf(geom="step") +
+        theme_classic()+
+        theme(
+            legend.position="right",
+            axis.text.x = element_text(
+                angle = 0,
+                hjust = 0.7,
+                size=12
+            ),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"),
+            axis.text.y = element_text(size=12), 
+            axis.title.x = element_text(size=12),
+            axis.title.y = element_text(size=12),
+            legend.text = element_text(size=12),
+            legend.title = element_text(size=12)
+        )+
+        labs(
+            x="Walltime",
+            y="CDF (%)",
+            color="Scheduling Type",
+            linetype="GPU"
+        )+
+        scale_color_d3()
+    
+    #######################################
+    ## Plot the delay cdf graph ##
+    #######################################
+    delayCDFPlot <- ggplot(data=dt, aes( delay, color= type))+
+        stat_ecdf(geom="step") +
+        theme_classic()+
+        theme(
+            legend.position="right",
+            axis.text.x = element_text(
+                angle = 0,
+                hjust = 0.7,
+                size=12
+            ),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"),
+            axis.text.y = element_text(size=12), 
+            axis.title.x = element_text(size=12),
+            axis.title.y = element_text(size=12),
+            legend.text = element_text(size=12),
+            legend.title = element_text(size=12)
+        )+
+        labs(
+            x="Delay (Events)",
+            y="CDF (%)",
+            color="Scheduling Type"
+        )+
+        scale_color_d3()
+    
+    #######################################
+    ## Plot the walltime by delay graph ##
+    #######################################
+    walltimeByDelayPlot <- ggplot(data=dt, aes( x = walltime, color= type, linetype = gpu))+
+        stat_ecdf()+
+        theme_classic()+
+        theme(
+            legend.position="right",
+            axis.text.x = element_text(
+                angle = 0,
+                hjust = 0.7,
+                size=12
+            ),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"),
+            axis.text.y = element_text(size=12), 
+            axis.title.x = element_text(size=12),
+            axis.title.y = element_text(size=12),
+            legend.text = element_text(size=12),
+            legend.title = element_text(size=12)
+        )+
+        labs(
+            x= "Delay (Events)",
+            y="CDF (%)",
+            color="Scheduling Type",
+            linetype = "GPU"
+        )+
+        scale_color_d3()
+    
+    ###################################
+    ## Plot all the graphs
+    ###################################
+    
+    tiff(sprintf("walltime_cdf_%s.tiff",size),width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
+    plot(walltimeCDFPlot)
+    dev.off()
+    
+    tiff(sprintf("delay_cdf_%s.tiff",size),width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
+    plot(delayCDFPlot)
+    dev.off()
+    
+    tiff(sprintf("walltime_delay_%s.tiff",size),width= 3500, height= 1200, units="px", res=400,compression = 'lzw')
+    plot(walltimeByDelayPlot)
+    dev.off()
+}
 #################################################
 ## CONVERTING THE FINAL GRAPHS INTO PNG AND EPS##
 #################################################
