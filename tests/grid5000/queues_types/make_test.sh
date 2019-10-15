0cff51799118157f9db6777a4b19026ed71453d8
Leonardo Rosa Rodrigues <leu_rr1@hotmail.com>
2019-10-16 01:51:04 +0200

    update test with 1k and 10k normalized time

--- tests/grid5000/queues_types/make_test.sh
+++ tests/grid5000/queues_types/make_test.sh
@@ -1,9 +1,9 @@
-cd ./../../bin/;
+cd ./../../../bin/;
 
 for site in "lyon"; do
     for onoff in {"online","offline"}; do
         for queue in {"fcfs","spf","sqfmin","safmin"}; do
-            for size in {"150k","100k","50k"}; do
+            for size in {"1k","10k","50k","100k","150k"}; do
                 echo "Executing scheduler with file $site-$size, type $onoff and queue $queue"
                 ./gpu_scheduler.out --test 5 --data-type flat -c none --file_name $site-$size -r ahpg --scheduling_type $onoff --debug critical --cmp $queue;
             done;
