6f489ce986b195becfac731ad2aa3954613a68db
leonardo <leu_rr1@hotmail.com>
2019-10-17 23:12:20 -0300

    update

--- tests/genericDC/on_off/make_standard_test.sh
+++ tests/genericDC/on_off/make_standard_test.sh
@@ -5,6 +5,6 @@ DC="LLNL_Thunder"
 for onoff in {"offline","online"}; do
     for site in $DC; do
         echo "Executing scheduler with file $size and type $onoff on site $site"
-        ./gpu_scheduler.out --test 5 --data-type flat -c none --file_name $site -r ahpg --scheduling_type $onoff --debug info;
+        ./gpu_scheduler.out --test 5 --data-type flat -c none --file_name $site -r ahpg --scheduling_type $onoff --debug critical;
     done;
 done;
